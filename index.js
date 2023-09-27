e1b53e6611712a051c69891abae0cf50ee93d1bf
Daniel-Nlemedim <Nzubenlems2002@gmail.com>
2023-09-27 13:04:22 +0100

    file updated

---
+++ index.js
@@ -0,0 +1,43 @@
+"use strict";
+
+const btnOpenModal = document.querySelector(".show-modal");
+const modal = document.querySelector(".modal");
+const hidden = document.querySelector(".hidden");
+const btnCloseModal = document.querySelector(".close-modal");
+const overlay = document.querySelector(".overlay");
+
+//function
+const openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const closeModal = function() {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+}
+
+btnOpenModal.addEventListener("click", openModal);
+ btnCloseModal.addEventListener("click", closeModal );
+ overlay.addEventListener("click", closeModal);
+
+
+
+
+//for the slide show
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 3000); // Change image every 3 seconds
+}
