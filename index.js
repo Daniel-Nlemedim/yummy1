ac6d70f9f44ba4fd6a974fec1d19f138d8090b63
Daniel-Nlemedim <Nzubenlems2002@gmail.com>
2023-11-01 16:32:06 +0100

    checking for ease style

--- index.js
+++ index.js
@@ -38,16 +38,3 @@ function showSlides() {
   slides[slideIndex - 1].style.display = "block";
   setTimeout(showSlides, 3000); // Change image every 3 seconds
 }
-
-//sticky navbar
-window.onscroll = function() {stickyNavbar()}
-const sticky = navbar.offsetTop;
-function stickyNavbar(){
-  if(window.pageYOffset >= sticky){
-    navbar.classList.add('sticky')
-  }else{
-    navbar.classList.remove('sticky')
-  }
-}
-
-
