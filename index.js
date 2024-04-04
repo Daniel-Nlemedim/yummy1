24ab3070455f149fff0141b13a67ec82e749ee53
Daniel-Nlemedim <Nzubenlems2002@gmail.com>
2024-04-04 17:32:10 +0100

    changes on the js file

--- index.js
+++ index.js
@@ -1,37 +1,46 @@
 "use strict";
-document.addEventListener("DOMContentLoaded", function () {
-  const btnOpenModal = document.querySelector(".show-modal");
-  const modal = document.querySelector(".modal");
-  const hidden = document.querySelector(".hidden");
-  const btnCloseModal = document.querySelector(".close-modal");
-  const overlay = document.querySelector(".overlay");
-  const copyYear = document.querySelector(".copy-year");
 
-  //function
-  const openModal = function () {
-    modal.classList.remove("hidden");
-    overlay.classList.remove("hidden");
-  };
+const copyYear = document.querySelector(".copy-year");
 
-  const closeModal = function () {
-    modal.classList.add("hidden");
-    overlay.classList.add("hidden");
-  };
+//function
 
-  btnOpenModal.addEventListener("click", openModal);
-  btnCloseModal.addEventListener("click", closeModal);
-  overlay.addEventListener("click", closeModal);
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".btn--close-modal");
+const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
 
-  const calcYear = function(){
-    const currentDate = new Date();
+const openModal = function (e) {
+  e.preventDefault();
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
 
-    const year = currentDate.getFullYear();
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
 
-    copyYear.textContent = year;
-  }
-  calcYear();
-
+btnsOpenModal.forEach(function (btn) {
+  return btn.addEventListener("click", openModal);
 });
+
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
+
+const calcYear = function () {
+  const currentDate = new Date();
+
+  const year = currentDate.getFullYear();
+
+  copyYear.textContent = year;
+};
+calcYear();
 
 //for the slide show
 let slideIndex = 0;
@@ -50,3 +59,12 @@ function showSlides() {
   slides[slideIndex - 1].style.display = "block";
   setTimeout(showSlides, 3000); // Change image every 3 seconds
 }
+
+document.querySelector(".nav__link").addEventListener("click", function (e) {
+  e.preventDefault();
+  if (e.target.classList.contains("nav-link")) {
+    const id = e.target.getAttribute("href");
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+  }
+  console.log('click',e.target)
+});
