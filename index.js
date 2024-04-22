281f45baed80c43f2dbaf727142d9e196f9b235a
Daniel-Nlemedim <Nzubenlems2002@gmail.com>
2024-04-22 20:03:17 +0100

    deleted some unused codes

--- index.js
+++ index.js
@@ -1,38 +1,10 @@
 "use strict";
 const header = document.querySelector(".header");
 const nav = document.querySelector(".navbar");
-const modal = document.querySelector(".modal");
-const overlay = document.querySelector(".overlay");
-const btnCloseModal = document.querySelector(".btn--close-modal");
-const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
 const section1 = document.querySelector(".firstCoord");
 const copyYear = document.querySelector(".copy-year");
 
 //function
-
-const openModal = function (e) {
-  e.preventDefault();
-  modal.classList.remove("hidden");
-  overlay.classList.remove("hidden");
-};
-
-const closeModal = function () {
-  modal.classList.add("hidden");
-  overlay.classList.add("hidden");
-};
-
-btnsOpenModal.forEach(function (btn) {
-  return btn.addEventListener("click", openModal);
-});
-
-btnCloseModal.addEventListener("click", closeModal);
-overlay.addEventListener("click", closeModal);
-
-document.addEventListener("keydown", function (e) {
-  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
-    closeModal();
-  }
-});
 
 //////////////////////////////////////
 //for the slide show
